9864a5fb719ce95542c78e160874bac92776923f
HessamCheraghi <hessam.ch.rose@gmail.com>
2022-03-26 17:26:23 +0430

    finished world map

--- src/components/WorldMap/WorldMap.jsx
+++ src/components/WorldMap/WorldMap.jsx
@@ -1,5 +1,14 @@
-// import styles from "./WorldMap.module.scss";
-
+import styles from "./WorldMap.module.scss";
+import map from "images/WorldMap.svg";
 export default function WorldMap() {
-  return <div>WorldMap</div>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.subheading}>ما همه جا هستیم</p>
+      <h2 className={styles.secondary}>
+        در هر کجای دنیا که باشید میتوانید از خدمات ما استفاده کنید ما محدود به
+        مرز ها نیستیم!
+      </h2>
+      <img className={styles.map} src={map} alt="نقشه جهان" />
+    </div>
+  );
 }
