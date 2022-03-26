11c7743a971517e0d7cc472a46a7d39e376ea586
HessamCheraghi <hessam.ch.rose@gmail.com>
2022-03-26 11:00:23 +0430

    finished Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,5 @@
 import styles from "./Hero.module.scss";
 
-import TextBox from "./TextBox";
-
 import HeroPNG from "images/Hero.png";
 import HeroWEBP from "images/Hero.webp";
 
@@ -9,8 +7,22 @@ export default function Hero() {
   return (
     <div className={styles.background}>
       <div className={styles.container}>
-        <TextBox />
-        <div className={styles.imgBox}>
+        <div className={styles.textBox}>
+          <h1 className={styles.header}>
+            هر جا که به کمک فوری نیاز داشتید
+            <br />
+            ما اونجا هستیم
+          </h1>
+          <p className={styles.subheader}>
+            بارها شده که وسیله نقلیه ما وسط راه از کار افتاده و توی راه موندیم
+            دیگه نگران این وضعیت نباشید ما اینجا هستیم تا به شما کمک کنیم. با
+            الو بنزین هیچوقت نگران توی راه بنزین تموم کردن توی راه نباشید
+          </p>
+          <button className={styles.download}>دانلود اپلیکیشن</button>
+        </div>
+      </div>
+      <div className={styles.imgBox}>
+        <div className={styles.blurImg}>
           <picture>
             <source srcSet={HeroWEBP} type="image/webp" />
             <img
