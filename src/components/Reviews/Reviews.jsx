480047fe935b61f679d73d27186902e838426791
HessamCheraghi <hessam.ch.rose@gmail.com>
2022-03-27 09:47:39 +0430

    finished reviews section

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,24 @@
-// import styles from "./Reviews.module.scss";
-
+import styles from "./Reviews.module.scss";
+import data from "./data";
 export default function Reviews() {
-  return <div>Reviews</div>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.subheading}>نظرات مشتری</p>
+      <h2 className={styles.secondary}>مشتریان سابق درباره ما چی میگن؟</h2>
+      <div className={styles.cardContainer}>
+        {data.map((datum) => (
+          <div key={datum.id} className={styles.card}>
+            <div className={styles.avatarBox}>
+              <div className={styles.avatar}>
+                <img src={datum.avatar} alt="avatar" />
+              </div>
+              <p className={styles.name}>{datum.name}</p>
+            </div>
+            <p className={styles.text}>{datum.review}</p>
+            <img className={styles.rate} src={datum.rate} alt="rating" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
