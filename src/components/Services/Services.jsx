3a1978cd7d04a7da081da9c77f250adeca19b160
HessamCheraghi <hessam.ch.rose@gmail.com>
2022-03-27 11:32:48 +0430

    finished services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,27 @@
-// import styles from "./Services.module.scss";
-
+import styles from "./Services.module.scss";
+import service1 from "images/Service1.png";
+import service2 from "images/Service2.png";
+import service3 from "images/Service3.png";
+import service4 from "images/Service4.png";
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.subheading}>خدمات</p>
+      <h2 className={styles.secondary}>راه حل ایده آل برای وسایل نقلیه شما</h2>
+      <div className={styles.grid}>
+        <div className={styles.item}>
+          <img src={service2} alt="" />
+        </div>
+        <div className={styles.item}>
+          <img src={service1} alt="" />
+        </div>
+        <div className={styles.item}>
+          <img src={service3} alt="" />
+        </div>
+        <div className={styles.item}>
+          <img src={service4} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
