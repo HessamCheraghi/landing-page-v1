c8720899baee569487c62327da9854276dcffd35
HessamCheraghi <hessam.ch.rose@gmail.com>
2022-05-05 09:53:57 +0430

    make an animation for navLinks

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,6 +1,7 @@
 import styles from "./Nav.module.scss";
+import { motion } from "framer-motion";
 
-export default function Nav() {
+export default function Nav({ delay }) {
   const list = [
     { label: "امکانات", href: "#" },
     { label: "خدمات", href: "#" },
@@ -8,21 +9,71 @@ export default function Nav() {
     { label: "سوالات متداول", href: "#" },
     { label: "تماس با ما", href: "#" },
   ];
+  // hover effects
+  const handelHover = function (e) {
+    if (e.target.tagName.toLowerCase() === "a") {
+      const link = e.target;
+      const siblings = link.closest("nav").querySelectorAll("a");
+
+      siblings.forEach((el) => {
+        if (el !== link) {
+          el.style.opacity = this;
+        }
+      });
+    }
+  };
+
+  // animations
+  const animationDelay = delay ?? 0;
+  const animateLinks = {
+    visible: (i) => ({
+      y: 0,
+      transition: {
+        delay: i * 0.2 + animationDelay,
+      },
+    }),
+    hidden: { y: "-200%" },
+  };
+  const animateButton = {
+    visible: {
+      y: 0,
+      transition: {
+        delay: list.length * 0.2,
+        duration: 0.15,
+      },
+    },
+    hidden: { y: "-200%" },
+  };
 
   return (
-    <nav className={styles.main}>
+    <nav
+      onMouseOver={handelHover.bind(0.3)}
+      onMouseOut={handelHover.bind(1)}
+      className={styles.main}
+    >
       <ul className={styles.list}>
-        {list.map((item) => (
-          <li key={item.label}>
+        {list.map((item, i) => (
+          <motion.li
+            custom={i}
+            initial="hidden"
+            animate="visible"
+            variants={animateLinks}
+            key={item.label}
+          >
             <a href={item.href} className={styles.link}>
               {item.label}
             </a>
-          </li>
+          </motion.li>
         ))}
         <li>
-          <button className={styles.download}>
+          <motion.button
+            initial="hidden"
+            animate="visible"
+            variants={animateButton}
+            className={styles.download}
+          >
             دانلود اپلیکیشن برای موبایل
-          </button>
+          </motion.button>
         </li>
       </ul>
     </nav>
